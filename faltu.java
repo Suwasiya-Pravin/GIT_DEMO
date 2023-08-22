7e80d8bdff3b2bc2627233068060b818fb505d34
Pravin Suwasiya <pravinsuwasiya45@gmail.com>
2023-08-22 20:31:51 +0530

    all file commited in dev

--- faltu.java
+++ faltu.java
@@ -1,4 +1,4 @@
-public class hello{
+public class faltu{
     public static void main(String[] args){
        System.out.println("this is faltu java file");
     }
--- newHello.java
+++ newHello.java
@@ -1,4 +1,4 @@
-public class hello{
+public class newHello{
     public static void main(String[] args){
         System.out.println("new hello");
        
